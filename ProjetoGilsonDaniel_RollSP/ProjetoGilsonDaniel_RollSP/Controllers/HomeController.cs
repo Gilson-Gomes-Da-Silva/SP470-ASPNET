7da1ce69af4d7bc6e6d9c2e62ec4112ba8f381f3
Gilson Gomes da Silva <ggison862@gmail.com>
2024-06-18 19:03:16 -0300

    Adicionando os arquivos.

---
+++ ProjetoGilsonDaniel_RollSP/ProjetoGilsonDaniel_RollSP/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjetoGilsonDaniel_RollSP.Models;
+using System.Diagnostics;
+
+namespace ProjetoGilsonDaniel_RollSP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Acervo()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
